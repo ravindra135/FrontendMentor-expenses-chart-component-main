9bc66c1f1bd0ff0a558a5a68c39895bf8e998355
Ravindra Reddy <reddyravindra947@gmail.com>
2022-08-24 19:14:23 +0530

    design ready

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "soft-red": "var(--softRed)",
+        "cyan": "var(--cyan)",
+        "darkBrown": "var(--darkBrown)",
+        "mediumBrown": "var(--mediumBrown)",
+        "cream": "var(--cream)",
+        "veryPaleOrange": "var(--veryPaleOrange)"
+      },
+    },
   },
   plugins: [],
 }
